f7546ba19a16f1bb29b408f29ad09ec7609d3e2f
brayan2302B <brayan85740189@gmail.com>
2026-05-26 15:18:30 -0500

    feat: modulos personas, areas y rol generados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PersonasModule } from './personas/personas.module';
+import { AreasModule } from './areas/areas.module';
+import { RolModule } from './rol/rol.module';
 
 @Module({
-  imports: [],
+  imports: [PersonasModule, AreasModule, RolModule],
   controllers: [AppController],
   providers: [AppService],
 })
