8de3d4792fd99be1b8390e9a87c594a9cd068b5e
brayan2302B <brayan85740189@gmail.com>
2026-05-26 16:45:17 -0500

    feat: DTOs de rol, areas y personas configurados

--- src/areas/dto/create-area.dto.ts
+++ src/areas/dto/create-area.dto.ts
@@ -1,1 +1,10 @@
-export class CreateAreaDto {}
+import { IsNumber, IsString, MaxLength } from 'class-validator';
+
+export class CreateAreaDto {
+  @IsString()
+  @MaxLength(100)
+  nombre!: string;
+
+  @IsNumber()
+  fk_rol!: number;
+}
--- src/rol/dto/create-rol.dto.ts
+++ src/rol/dto/create-rol.dto.ts
@@ -1,1 +1,15 @@
-export class CreateRolDto {}
+import { IsBoolean, IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class CreateRolDto {
+  @IsString()
+  @MaxLength(50)
+  estado!: string;
+
+  @IsBoolean()
+  @IsOptional()
+  inactivo?: boolean;
+
+  @IsBoolean()
+  @IsOptional()
+  activo?: boolean;
+}
