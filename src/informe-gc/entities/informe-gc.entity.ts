b4df0814591f73c93b05c543f7f78e4766ed5aa9
Jhon Claros <jhonclaros482@gmail.com>
2026-05-26 20:55:09 -0500

    feat(informe-gc): se define la entidad con sus columnas y relaciones usando TypeORM

--- src/informe-gc/entities/informe-gc.entity.ts
+++ src/informe-gc/entities/informe-gc.entity.ts
@@ -1,1 +1,18 @@
-export class InformeGc {}
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Actividad } from '../../actividades/entities/actividad.entity';
+
+@Entity('informe_gc')
+export class InformeGc {
+
+  @PrimaryGeneratedColumn()
+  id_gc: number;
+
+  @Column({ type: 'varchar', length: 50 })
+  version_gc: string;
+
+  @Column({ type: 'int', nullable: true })
+  fk_informe: number;
+
+  @OneToMany(() => Actividad, (actividad) => actividad.informeGc)
+  actividades: Actividad[];
+}
