3a66f24c69c680bf615394ef67fc4132a1cb921d
Jhon Claros <jhonclaros482@gmail.com>
2026-05-26 21:24:22 -0500

    feat(actividades): se agregan los endpoints REST en el controlador

--- src/actividades/actividades.controller.ts
+++ src/actividades/actividades.controller.ts
@@ -1,15 +1,16 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ActividadesService } from './actividades.service';
-import { CreateActividadeDto } from './dto/create-actividade.dto';
-import { UpdateActividadeDto } from './dto/update-actividade.dto';
+import { CreateActividadDto } from './dto/create-actividade.dto';
+import { UpdateActividadDto } from './dto/update-actividade.dto';
 
 @Controller('actividades')
 export class ActividadesController {
+
   constructor(private readonly actividadesService: ActividadesService) {}
 
   @Post()
-  create(@Body() createActividadeDto: CreateActividadeDto) {
-    return this.actividadesService.create(createActividadeDto);
+  create(@Body() createActividadDto: CreateActividadDto) {
+    return this.actividadesService.create(createActividadDto);
   }
 
   @Get()
@@ -23,8 +24,8 @@ export class ActividadesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateActividadeDto: UpdateActividadeDto) {
-    return this.actividadesService.update(+id, updateActividadeDto);
+  update(@Param('id') id: string, @Body() updateActividadDto: UpdateActividadDto) {
+    return this.actividadesService.update(+id, updateActividadDto);
   }
 
   @Delete(':id')
