8c7bb8c27d7301929bd7711785709c8250e425dc
Gxmez20 <kg1389608@gmail.com>
2026-05-26 21:23:05 -0500

    feat: entidad contrato creada

--- src/contratos/entities/contrato.entity.ts
+++ src/contratos/entities/contrato.entity.ts
@@ -1,1 +1,19 @@
-export class Contrato {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('contratos')
+export class Contrato {
+  @PrimaryGeneratedColumn()
+  id_contrato!: number;
+
+  @Column({ type: 'date' })
+  fecha_inicio!: Date;
+
+  @Column({ type: 'date' })
+  fecha_fin!: Date;
+
+  @Column()
+  estado!: string;
+
+  @Column()
+  fk_persona!: number;
+}
