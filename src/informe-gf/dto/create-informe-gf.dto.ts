814de53030dbdc054f61ca16f2000221187944b1
Jhon Claros <jhonclaros482@gmail.com>
2026-05-26 21:09:25 -0500

    feat(informe-gf): se agregan los DTOs de creación y actualización con sus validaciones

--- src/informe-gf/dto/create-informe-gf.dto.ts
+++ src/informe-gf/dto/create-informe-gf.dto.ts
@@ -1,1 +1,12 @@
-export class CreateInformeGfDto {}
+import { IsInt, IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class CreateInformeGfDto {
+
+  @IsString()
+  @MaxLength(50)
+  version_gf: string;
+
+  @IsInt()
+  @IsOptional()
+  fk_informe?: number;
+}
