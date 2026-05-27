5aa425fa357dfd4b97752fe8aaab3e0f2d29c220
Jhon Claros <jhonclaros482@gmail.com>
2026-05-26 20:58:34 -0500

    feat(informe-gf): se define la entidad con sus columnas y relaciones usando TypeORM

--- src/informe-gf/entities/informe-gf.entity.ts
+++ src/informe-gf/entities/informe-gf.entity.ts
@@ -1,1 +1,14 @@
-export class InformeGf {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('informe_gf')
+export class InformeGf {
+
+  @PrimaryGeneratedColumn()
+  id_gf: number;
+
+  @Column({ type: 'varchar', length: 50 })
+  version_gf: string;
+
+  @Column({ type: 'int', nullable: true })
+  fk_informe: number;
+}
