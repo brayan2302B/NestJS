02803b9be7a2ccd6e7583da7ae253fc2fc4fce53
Gxmez20 <kg1389608@gmail.com>
2026-05-26 21:30:08 -0500

    feat: module de contratos configurado

--- src/contratos/contratos.module.ts
+++ src/contratos/contratos.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContratosService } from './contratos.service';
 import { ContratosController } from './contratos.controller';
+import { Contrato } from './entities/contrato.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Contrato])],
   controllers: [ContratosController],
   providers: [ContratosService],
 })
