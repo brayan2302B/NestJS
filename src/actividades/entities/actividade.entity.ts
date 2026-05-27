f099b1655cfa220625195a9a278d75217bdfdd4f
Jhon Claros <jhonclaros482@gmail.com>
2026-05-26 21:00:37 -0500

    feat(actividades): se define la entidad con sus columnas y relaciones usando TypeORM

--- src/actividades/entities/actividade.entity.ts
+++ src/actividades/entities/actividade.entity.ts
@@ -1,1 +1,35 @@
-export class Actividade {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany, JoinColumn } from 'typeorm';
+import { InformeGc } from '../../informe-gc/entities/informe-gc.entity';
+import { Evidencia } from '../../evidencias/entities/evidencia.entity';
+
+@Entity('actividades')
+export class Actividad {
+
+  @PrimaryGeneratedColumn()
+  id_actividad: number;
+
+  @Column({ type: 'date' })
+  fecha: Date;
+
+  @Column({ type: 'varchar', length: 100 })
+  competencia: string;
+
+  @Column({ type: 'varchar', length: 3 })
+  estado: string;
+
+  @Column({ type: 'date' })
+  fecha_fin: Date;
+
+  @Column({ type: 'date' })
+  fecha_inicio: Date;
+
+  @Column({ type: 'text' })
+  resultado: string;
+
+  @ManyToOne(() => InformeGc, (informeGc) => informeGc.actividades, { nullable: true })
+  @JoinColumn({ name: 'fk_gc' })
+  informeGc: InformeGc;
+
+  @OneToMany(() => Evidencia, (evidencia) => evidencia.actividad)
+  evidencias: Evidencia[];
+}
