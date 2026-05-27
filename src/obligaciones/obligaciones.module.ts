dbb4eff2a5a0826cf4b5f7ed523084017d88c6f8
Gxmez20 <kg1389608@gmail.com>
2026-05-26 21:35:48 -0500

    feat: module de obligaciones configurado

--- src/obligaciones/obligaciones.module.ts
+++ src/obligaciones/obligaciones.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ObligacionesService } from './obligaciones.service';
 import { ObligacionesController } from './obligaciones.controller';
+import { Obligacione } from './entities/obligacione.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Obligacione])],
   controllers: [ObligacionesController],
   providers: [ObligacionesService],
 })
