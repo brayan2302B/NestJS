932c5dc9a0e0a8309102eede3923256fbad0163a
Jhon Claros <jhonclaros482@gmail.com>
2026-05-26 21:12:05 -0500

    feat(evidencias): se agregan los DTOs de creación y actualización con sus validaciones

--- src/evidencias/dto/create-evidencia.dto.ts
+++ src/evidencias/dto/create-evidencia.dto.ts
@@ -1,1 +1,19 @@
-export class CreateEvidenciaDto {}
+import { IsInt, IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class CreateEvidenciaDto {
+
+  @IsString()
+  descripcion: string;
+
+  @IsString()
+  @MaxLength(255)
+  carpeta_obligacion: string;
+
+  @IsString()
+  @MaxLength(215)
+  fotografia: string;
+
+  @IsInt()
+  @IsOptional()
+  fk_actividades?: number;
+}
