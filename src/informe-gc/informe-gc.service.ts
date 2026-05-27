71cea66e8069aaaa1ef6e7b1c70d98e0c22b17d3
Jhon Claros <jhonclaros482@gmail.com>
2026-05-26 21:13:28 -0500

    feat(informe-gc): se implementan los métodos CRUD en el servicio

--- src/informe-gc/informe-gc.service.ts
+++ src/informe-gc/informe-gc.service.ts
@@ -1,26 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { InformeGc } from './entities/informe-gc.entity';
 import { CreateInformeGcDto } from './dto/create-informe-gc.dto';
 import { UpdateInformeGcDto } from './dto/update-informe-gc.dto';
 
 @Injectable()
 export class InformeGcService {
+
+  constructor(
+    @InjectRepository(InformeGc)
+    private readonly informeGcRepository: Repository<InformeGc>,
+  ) {}
+
   create(createInformeGcDto: CreateInformeGcDto) {
-    return 'This action adds a new informeGc';
+    const informe = this.informeGcRepository.create(createInformeGcDto);
+    return this.informeGcRepository.save(informe);
   }
 
   findAll() {
-    return `This action returns all informeGc`;
+    return this.informeGcRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} informeGc`;
+  async findOne(id: number) {
+    const informe = await this.informeGcRepository.findOneBy({ id_gc: id });
+    if (!informe) throw new NotFoundException(`InformeGc #${id} no encontrado`);
+    return informe;
   }
 
-  update(id: number, updateInformeGcDto: UpdateInformeGcDto) {
-    return `This action updates a #${id} informeGc`;
+  async update(id: number, updateInformeGcDto: UpdateInformeGcDto) {
+    await this.findOne(id);
+    await this.informeGcRepository.update(id, updateInformeGcDto);
+    return this.findOne(id);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} informeGc`;
+  async remove(id: number) {
+    await this.findOne(id);
+    return this.informeGcRepository.delete(id);
   }
 }
