2f7ffc444e32023376cab01baa5986553b020227
Carlos <c4798175@gmail.com>
2026-05-26 19:09:00 -0500

    feat: modulos versiones, informes y novedades configurados

--- src/versiones/versiones.module.ts
+++ src/versiones/versiones.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { VersionesService } from './versiones.service';
 import { VersionesController } from './versiones.controller';
+import { Version } from './entities/version.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Version])],
   controllers: [VersionesController],
   providers: [VersionesService],
+  exports: [TypeOrmModule],
 })
 export class VersionesModule {}
