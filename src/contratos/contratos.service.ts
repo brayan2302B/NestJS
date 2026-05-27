a693cbe068476797614196574b87e646f1a4cee1
Gxmez20 <kg1389608@gmail.com>
2026-05-26 21:29:18 -0500

    feat: service de contratos creado

--- src/contratos/contratos.service.ts
+++ src/contratos/contratos.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Contrato } from './entities/contrato.entity';
 import { CreateContratoDto } from './dto/create-contrato.dto';
-import { UpdateContratoDto } from './dto/update-contrato.dto';
 
 @Injectable()
 export class ContratosService {
+  constructor(
+    @InjectRepository(Contrato)
+    private contratosRepository: Repository<Contrato>,
+  ) {}
+
   create(createContratoDto: CreateContratoDto) {
-    return 'This action adds a new contrato';
+    const contrato = this.contratosRepository.create(createContratoDto);
+    return this.contratosRepository.save(contrato);
   }
 
   findAll() {
-    return `This action returns all contratos`;
+    return this.contratosRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} contrato`;
+    return this.contratosRepository.findOneBy({ id_contrato: id });
   }
 
-  update(id: number, updateContratoDto: UpdateContratoDto) {
-    return `This action updates a #${id} contrato`;
+  update(id: number, updateContratoDto: Partial<CreateContratoDto>) {
+    return this.contratosRepository.update(id, updateContratoDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} contrato`;
+    return this.contratosRepository.delete(id);
   }
 }
