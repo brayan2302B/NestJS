d5ea2ee52ec5aae598bb156704a01a88114e31f5
Gxmez20 <kg1389608@gmail.com>
2026-05-26 21:32:11 -0500

    feat: entidad obligaciones creada

--- src/obligaciones/entities/obligacione.entity.ts
+++ src/obligaciones/entities/obligacione.entity.ts
@@ -1,1 +1,13 @@
-export class Obligacione {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('obligaciones')
+export class Obligacione {
+  @PrimaryGeneratedColumn()
+  id_obligaciones!: number;
+
+  @Column()
+  descripcion!: string;
+
+  @Column()
+  fk_contrato!: number;
+}
