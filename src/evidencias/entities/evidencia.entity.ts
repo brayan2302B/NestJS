38d75d8a3a1cef9d64487fa0396d7eb581b4ebee
Jhon Claros <jhonclaros482@gmail.com>
2026-05-26 21:03:40 -0500

    feat(evidencias): se define la entidad con sus columnas y relaciones usando TypeORM

--- src/evidencias/entities/evidencia.entity.ts
+++ src/evidencias/entities/evidencia.entity.ts
@@ -1,1 +1,22 @@
-export class Evidencia {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { Actividad } from '../../actividades/entities/actividad.entity';
+
+@Entity('evidencias')
+export class Evidencia {
+
+  @PrimaryGeneratedColumn()
+  id_evidencia: number;
+
+  @Column({ type: 'text' })
+  descripcion: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  carpeta_obligacion: string;
+
+  @Column({ type: 'varchar', length: 215 })
+  fotografia: string;
+
+  @ManyToOne(() => Actividad, (actividad) => actividad.evidencias, { nullable: true })
+  @JoinColumn({ name: 'fk_actividades' })
+  actividad: Actividad;
+}
